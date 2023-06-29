2c1a5075ac31f3b79e02e05ae9e2391dd9d290ee
Hiul <hiufailau.41@gmail.com>
2023-06-29 13:20:13 +0200

    added challenge-6

---
+++ number-to-string.js
@@ -0,0 +1,21 @@
+// transform a number (integer) into a string
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return (num = `${num}`);
+}
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return (num = num + '');
+}
