d35b7dea42b1850fa3add7b6795e46c81d1ed168
Hiul <hiufailau.41@gmail.com>
2023-07-09 15:09:14 +0200

    added new challenge

--- 13-reverse-words-in-sentence.js
+++ 13-reverse-words-in-sentence.js
@@ -1,12 +1,12 @@
 // function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
-var x = 'This is an example!';
+var x = "This is an example!";
 
 function reverseWords(str) {
-  var splitStr = str.split(' ');
+  var splitStr = str.split(" ");
   var reverseArr = splitStr.map(function (word) {
-    return word.split('').reverse().join('');
+    return word.split("").reverse().join("");
   });
-  return reverseArr.join(' ');
+  return reverseArr.join(" ");
 }
 
 console.log(reverseWords(x));
@@ -14,20 +14,20 @@ console.log(reverseWords(x));
 //(Best) Option 2:
 function reverseWords(str) {
   return str
-    .split(' ')
+    .split(" ")
     .map(function (word) {
-      return word.split('').reverse().join('');
+      return word.split("").reverse().join("");
     })
-    .join(' ');
+    .join(" ");
 }
 console.log(reverseWords(x));
 
 // Option 3:
 function reverseWords(str) {
   return str
-    .split(' ')
-    .map((str) => str.split('').reverse().join(''))
-    .join(' ');
+    .split(" ")
+    .map((str) => str.split("").reverse().join(""))
+    .join(" ");
 }
 console.log(reverseWords(x));
 
@@ -35,20 +35,20 @@ console.log(reverseWords(x));
 function reverseWords(str) {
   // Go for it
   //split words into seperate arrays
-  return str.split('').reverse().join('').split(' ').reverse().join(' ');
+  return str.split("").reverse().join("").split(" ").reverse().join(" ");
 }
 console.log(reverseWords(x));
 
 // Option 5:
 function reverseWords(str) {
-  let reversedWord = '';
-  let reversedStr = '';
+  let reversedWord = "";
+  let reversedStr = "";
   for (let i = 0; i < str.length; i++) {
-    if (str[i] !== ' ') {
+    if (str[i] !== " ") {
       reversedWord = str[i] + reversedWord;
     } else {
-      reversedStr += reversedWord + ' ';
-      reversedWord = '';
+      reversedStr += reversedWord + " ";
+      reversedWord = "";
     }
   }
   return reversedStr + reversedWord;
